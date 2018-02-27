839334ca0784ca2994b44be8f9f9ff27de31f02b
Jorge Oviedo <jorgeloviedo@hotmail.com>
2018-02-27 15:39:51 -0300

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='FlaskApp',
       version='1.0',
       description='A basic Flask app with static files',
-      author='Jorge Luis Oviedo',
+      author='Jorge Luis Oviedo II',
       author_email='jorgeloviedo@outlook.com',
       install_requires=['Flask>=0.10.1', 'gunicorn>=0.0.1'],
       )
